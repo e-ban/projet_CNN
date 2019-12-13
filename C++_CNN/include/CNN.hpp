ac20f94bdecfa65d0bb21fbdce4fe7e4bd32c7bd
e-ban <iban.guinebert@gmail.com>
2019-12-13 12:03:18 +0100

    dkdkd

--- C++_CNN/include/CNN.hpp
+++ C++_CNN/include/CNN.hpp
@@ -1,4 +1,15 @@
 #include <iostream>
+#include "imgproc_vfix_config.h"
 
 
+#include "ac_fixed.h"
+#include "ac_channel.h"
 
+typedef ac_fixed<IMGPROC_IMAGE_IN_P> din_type;
+typedef ac_fixed<IMGPROC_IMAGE_OUT_P> dout_type;
+
+
+int convolution(  din_type datain[IMGPROC_IMAGE_IN_SIZE],
+  c_type weights[IMGPROC_KERNEL_SIZE],
+  c_type biases[IMGPROC_IMAGE_OUT_SIZE_2],
+  dout_type dataout[IMGPROC_IMAGE_OUT_SIZE]);
