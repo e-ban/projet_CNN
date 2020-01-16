87ce2e75f45eec5fc57afb20a9b35a39e25dd3ed
e-ban <iban.guinebert@gmail.com>
2020-01-16 19:14:07 +0100

    top update

--- C++_CNN/src/TOP_CNN.cpp
+++ C++_CNN/src/TOP_CNN.cpp
@@ -4,16 +4,20 @@
 #include "convolutionReLU.h"
 #include "normalize.h"
 #include "loadPicture.h"
-
+#include "maxpool.h"
 #pragma hls_design top
 
 void CNN()
 {
-  static din_type input[IMGPROC_IMAGE_IN_SIZE];
-  static dout_type output[IMGPROC_CONV1_OUT_SIZE];
-  loadFilePGM(input);//load picture's data from pgm file
-  normalize(input,output);
-  savePicture("norm.pgm",output,0);
-  convolutionReLU1(input,conv1_weights,conv1_biases,output); //execute convolution 1 and store result in output
-  savePicture("conv1.pgm",output,1);
+  static din_type image[IMGPROC_IMAGE_IN_SIZE];
+  static din_type normOut[IMGPROC_IMAGE_IN_SIZE];
+  static dout_type conv1Out[IMGPROC_CONV1_OUT_SIZE];
+  static dout_type maxpoolOut[IMGPROC_CONV1_OUT_SIZE];
+
+  loadFilePGM(image);//load picture's data from pgm file
+  normalize(image,normOut);
+  savePicture("norm.pgm",normOut,0);
+  convolutionReLU1(normOut,conv1_weights,conv1_biases,conv1Out); //execute convolution 1 and store result in output
+  maxpool1(conv1Out,maxpoolOut);
+  savePicture("output.pgm",maxpoolOut,0);
 }
