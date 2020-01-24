98d9a1e1cdcb8bfddb2e131736acf1a2efbc58c4
Totsuka2169 <xph3sei705@>
2020-01-24 09:58:35 +0100

    correction sqrt edgedetector

--- EdgeDetector/edgeDetector.h
+++ EdgeDetector/edgeDetector.h
@@ -4,6 +4,7 @@
 #include "kernel.h"
 #include "convolutionSimple.h"
 #include "display.h"
+#include "ac_math.h"
 
 void ImgProcTest(CNN_DATA_OUT_TYPE img_in[CNN_IMAGE_IN_SIZE],CNN_DATA_OUT_TYPE img_out[CNN_IMAGE_IN_SIZE]);
 
