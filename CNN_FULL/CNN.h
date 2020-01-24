0b25b7ae68475848615a9ef5233af4f276d26d35
e-ban <iban.guinebert@gmail.com>
2020-01-24 15:39:51 +0100

    tout

---
+++ CNN_FULL/CNN.h
@@ -0,0 +1,13 @@
+#ifndef __CNN_H__
+#define __CNN_H__
+#include "types.h"
+#include "parameters.h"
+#include "convolutionReLU.h"
+#include "normalize.h"
+#include "maxpool.h"
+#include "perceptron.h"
+#include "display.h"
+
+void CNN(CNN_DATA_TYPE* mem1,CNN_DATA_TYPE* mem2,CNN_IMAGE_TYPE* imageOUT);
+
+#endif
---
+++ CNN_FULL/normalize.h
@@ -0,0 +1,8 @@
+
+#ifndef __NORMALIZE_H__
+#define __NORMALIZE_H__
+#include "types.h"
+#include "ac_math.h"
+
+void normalize(  CNN_DATA_TYPE imageIN[],  CNN_DATA_TYPE imageOUT[]);
+#endif
