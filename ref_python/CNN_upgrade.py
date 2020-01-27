cb0b8c053451475005522c72f414c5fbb090cc8f
e-ban <iban.guinebert@gmail.com>
2020-01-27 11:32:39 +0100

    image n

--- ref_python/CNN_upgrade.py
+++ ref_python/CNN_upgrade.py
@@ -261,9 +261,10 @@ def saveImageNorm():
     d=dicoCoeff.DicoCoeff("CNN_coeff_3x3.txt")
     cnn=CNN(d.dico)
     cnn.addNormalizeLayer()
-    result=cnn.run(centered_crop(image,24,24))
+    crop=centered_crop(image,24,24)
+    result=cnn.run(crop)
     print(result)
-    f=open("image.h","w")
+    f=open("imageNorm.h","w")
     f.write("CNN_DATA_TYPE imageNorm["+str(result.size)+"={")
     for line in range(result.shape[0]):
         for col in range(result.shape[1]):
@@ -273,8 +274,18 @@ def saveImageNorm():
         f.write("\n")
     f.write("};")
     f.close()
+    f=open("image.h","w")
+    f.write("CNN_DATA_TYPE imageIN["+str(result.size)+"={")
+    for line in range(crop.shape[0]):
+        for col in range(crop.shape[1]):
+            for can in range(crop.shape[2]):
+                f.write(str(crop[line,col,can]))
+                f.write(",")
+        f.write("\n")
+    f.write("};")
+    f.close()
     
 if __name__=="__main__":
-    
+    saveImageNorm()
     #testConvolution()
-    checkCifar10()
+    #checkCifar10()
