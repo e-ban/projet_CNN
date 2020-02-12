2fdd80cedce10b63d1639c3c543fb54b6953ef9d
e-ban <iban.guinebert@gmail.com>
2020-02-12 16:35:31 +0100

    modif edge Hls

--- code/CPP/PreHLS/EdgeDetector/convolutionSimple.cpp
+++ code/CPP/PreHLS/EdgeDetector/convolutionSimple.cpp
@@ -26,13 +26,13 @@ void convolutionReLU( CNN_DATA_OUT_TYPE datain[CNN_IMAGE_IN_SIZE], CNN_DATA_OUT_
 		  {
        	            if(row+kerH>=0 && row+kerH<height_in && col+kerW>=0 && col+kerW<width_in)
 		    {
-       	              s=s+(CNN_DATA_OUT_TYPE)datain[width_in*canal_in*(row+kerH)+canal_in*(col+kerW)+inChannel]*kernel[CNN_KERNEL_W*canal_in*canal_out*(kerH+1)+canal_in*canal_out*(kerW+1)+canal_out*inChannel+outChannel];
+       	              s=s+datain[width_in*canal_in*(row+kerH)+canal_in*(col+kerW)+inChannel]*kernel[CNN_KERNEL_W*canal_in*canal_out*(kerH+1)+canal_in*canal_out*(kerW+1)+canal_out*inChannel+outChannel];
        	            }
        	          }
        	        }
        	      }
        	      if(s<0)  s=0;
-       	      dataout[width_out*canal_out*row+canal_out*col+outChannel]=s;
+       	      dataout[width_out*canal_out*row+canal_out*col+outChannel]=(CNN_DATA_OUT_TYPE)s;
        	    }
       	  }
 	}
